e76ea0409f777f727c5662403c8dec99b602f453
x <exit>
2022-10-18 08:04:33 +0000

    Create development branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable linebreak-style */
-// import _ from 'lodash';
-// import './styles.css';
-// import 'https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js';
-
-// require('bootstrap-icons/font/bootstrap-icons.css');
