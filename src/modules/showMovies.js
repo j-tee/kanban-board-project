78b26178c890f9aeed7a2aef99bb446b8c03cb91
nahnahAJ <nahnahahjuwah@gmail.com>
2022-10-19 17:57:04 +0000

    Add dynamic modal content and enable popUp

--- src/modules/showMovies.js
+++ src/modules/showMovies.js
@@ -1,13 +1,41 @@
+const modal = document.getElementById('popUpModal')
 /* eslint-disable linebreak-style */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable linebreak-style */
 export default class ShowMovie {
   static sortingFunction = (a, b) => parseFloat(a.id) - parseFloat(b.id);
 
+  static createModal = (element) => {
+    const modalContent = document.createElement('div');
+        modalContent.innerHTML = `
+        <div class='modalStyle container'>
+            <img src="${element.image.medium}">
+            <span class="name">${element.name}</span>
+            <span>${element.summary}</span>
+            <div class="properties">
+            <span>Rating:${element.rating.average}</span>
+            <span>Status:${element.status}</span>
+            <span>Runtime:${element.runtime}</span>
+            <span>Show Time:${element.schedule.time}</span>
+            </div>
+        </div>        
+        <div>
+        <button type="button" class="closeBtn btn btn-outline-dark">X</button>
+        </div>
+        `
+        modal.innerHTML = '';
+        modal.appendChild(modalContent);
+
+        const closeBtn = document.querySelector('.closeBtn')
+        closeBtn.addEventListener('click', () => {
+          modal.className = 'hide';
+        })
+  } 
+
   // eslint-disable-next-line no-unused-vars
   static showMovies = (movies, display) => {
     const listOfMovies = movies.sort(this.sortingFunction);
-    display.innerHTML = '';
+    display.innerHTML = "";
     listOfMovies.forEach((element) => {
       const item = `  
       <div>
@@ -17,17 +45,20 @@ export default class ShowMovie {
           <li class="lang">${element.language}</li>
       </ul>        
       <div>
-      <button>Comments</button> <br>
+      <button id=${element.id}>Comments</button> <br>
       <button>Reservations</button>
       </div>
       </div>
       `;
-      display.insertAdjacentHTML('beforeend', item);
+      display.insertAdjacentHTML("beforeend", item);
+      // const popUpMenu = document.querySelector(".menu");
+      const popUpMenu = document.getElementById(`${element.id}`)
+      popUpMenu.addEventListener("click", () => 
+      {modal.className = 'show';
+        this.createModal(element)});
     });
-  }
+  };
 
   // eslint-disable-next-line no-unused-vars
-  static showError = (displayError) => {
-    
-  }
+  static showError = (displayError) => {};
 }
