821f923ee3c8e599aaf86212f11b38100d59826c
nahnahAJ <nahnahahjuwah@gmail.com>
2022-10-20 11:43:16 +0000

    Add funtions to add comments to the Api and Display added comment in the PopUp

--- src/modules/showMovies.js
+++ src/modules/showMovies.js
@@ -5,6 +5,59 @@ const modal = document.getElementById('popUpModal');
 export default class ShowMovie {
   static sortingFunction = (a, b) => parseFloat(a.id) - parseFloat(b.id);
 
+  static addComment = (id) => {
+    const formSubmit = document.getElementById('form');
+    const commentsContainer = document.querySelector('.displayComments');
+
+    // BhcsHKJV6Es48Aq8fpBk
+    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/bm0LLMjBZr50WwAoQBN0/comments';
+
+    formSubmit.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const userName = document.getElementById('name').value;
+      const insights = document.getElementById('insights').value;
+
+      // Create an object
+      const commentObject = {
+        item_id: id,
+        username: userName,
+        comment: insights,
+      };
+
+      // Sending comments to the API
+      const sendComment = async () => {
+        await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(commentObject),
+        });
+        commentsContainer.innerHTML = '';
+        this.displayComment(id);
+      };
+      sendComment();
+      formSubmit.reset();
+    });
+  };
+
+  static displayComment = async (id) => {
+    const url2 = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/bm0LLMjBZr50WwAoQBN0/comments?item_id=${id}`;
+
+    const commentsContainer = document.querySelector('.displayComments');
+    const res = await fetch(url2, {
+      method: 'GET',
+    });
+    const data = await res.json();
+      
+    return data.forEach((recorded) => {
+      //  create DOM elements and Append to display in the DOM
+      const list = document.createElement('li');
+      list.className = 'commentStyle';
+      list.innerHTML = `${recorded.creation_date}: ${recorded.username} : ${recorded.comment}`;
+      commentsContainer.appendChild(list);
+    });
+  };
+
   static createModal = (element) => {
     const modalContent = document.createElement('div');
     modalContent.innerHTML = `
@@ -22,6 +75,20 @@ export default class ShowMovie {
         <div>
         <button type="button" class="closeBtn btn btn-outline-dark">X</button>
         </div>
+        <div class="container displayComments"></div>
+        <form class="container form-style" id="form">
+        <div class="mb-3">
+          <label for="name" class="form-label"></label>
+          <input type="text" class="form-control" id="name" name="username" placeholder="Your name">
+        </div>
+        <div class="mb-3">
+          <label for="insights" class="form-label"></label>
+          <textarea class="form-control" id="insights" rows="3" name="comment" placeholder="Your insights"></textarea>
+        </div>
+        <div>
+        <button type="submit" class="btn btn-outline-success">Comment</button>
+        </div>
+        </form>
         `;
     modal.innerHTML = '';
     modal.appendChild(modalContent);
@@ -30,7 +97,9 @@ export default class ShowMovie {
     closeBtn.addEventListener('click', () => {
       modal.className = 'hide';
     });
-  } 
+    this.addComment(element.id);
+    this.displayComment(element.id);
+  };
 
   // eslint-disable-next-line no-unused-vars
   static showMovies = (movies, display) => {
@@ -51,11 +120,10 @@ export default class ShowMovie {
       </div>
       `;
       display.insertAdjacentHTML('beforeend', item);
-      // const popUpMenu = document.querySelector(".menu");
       const popUpMenu = document.getElementById(`${element.id}`);
       popUpMenu.addEventListener('click', () => {
         modal.className = 'show';
-        this.createModal(element); 
+        this.createModal(element);
       });
     });
   };
